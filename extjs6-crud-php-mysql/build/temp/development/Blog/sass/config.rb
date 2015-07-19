4b751592486cd1a50bf57c67d749227a5c4f97c2
Loiane Groner <loianeg@gmail.com>
2015-07-19 20:07:55 -0300

    init app - crud example

---
+++ extjs6-crud-php-mysql/build/temp/development/Blog/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Applications/XAMPP/xamppfiles/htdocs/extjs6-examples/extjs6-crud-php-mysql/build/.sass-cache'
---
+++ extjs6-crud-php-mysql/build/temp/development/Blog/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Applications/XAMPP/xamppfiles/htdocs/extjs6-examples/extjs6-crud-php-mysql/app.js
