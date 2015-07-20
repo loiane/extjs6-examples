3bdb2c923e4419f460379be8cd18813dcb49e807
Loiane Groner <loianeg@gmail.com>
2015-07-20 18:25:05 -0300

    crud modern in progress

---
+++ extjs6-crud-localstorage/modern/src/view/contacts/List.js
@@ -0,0 +1,74 @@
+Ext.define('Blog.view.contacts.List', {
+    extend: 'Ext.grid.Grid',
+
+    xtype: 'contactslist',
+
+    requires: [
+        'Blog.view.contacts.ContactsController',
+        'Blog.view.contacts.ContactsModel'
+    ],
+
+    viewModel: 'contacts',
+    controller: 'contacts',
+
+    iconCls: 'fa fa-users',
+
+    bind: {
+        title: '{title}',
+        store: '{contacts}'
+    },
+
+    columns: [
+        {
+            width: 50,
+            dataIndex: 'id',
+            text: '#'
+        },
+        {
+            width: 200,
+            dataIndex: 'name',
+            flex: 1,
+            text: 'Name'
+        },
+        {
+            width: 200,
+            dataIndex: 'phone',
+            text: 'Phone'
+        },
+        {
+            width: 250,
+            dataIndex: 'email',
+            text: 'Email'
+        }
+    ],
+
+    items: [
+        {
+            xtype: 'toolbar',
+            docked: 'top',
+            items: [
+                {
+                    text: 'Add',
+                    iconCls: 'fa fa-plus',
+                    listeners: {
+                        click: 'onAddClick'
+                    }
+                },
+                {
+                    text: 'Edit',
+                    iconCls: 'fa fa-pencil',
+                    listeners: {
+                        click: 'onEditClick'
+                    }
+                },
+                {
+                    text: 'Delete',
+                    iconCls: 'fa fa-trash',
+                    listeners: {
+                        tap: 'onDeleteClick'
+                    }
+                }
+            ]
+        }
+    ]
+});
